33c024018b2065d6ec4486f0983a8a9a06943f4a
kajaltyagi18 <kajaltyagi18aug@gmail.com>
2025-05-30 12:36:03 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.png" className="App-logo" alt="KaRa Vidya Clothing Logo" />
+        <h1>Coming Soon</h1>
+        <p>Elegance, reimagined in Ethnic</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
